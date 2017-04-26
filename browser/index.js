629e9ed53cfe874a02716e002ad6b3a3f9f167b1
Stanislav Synko <safitudo@gmail.com>
2017-04-25 22:46:50 -0400

    BIP32 implemented on high level, next step: get the balances properly

--- browser/index.js
+++ browser/index.js
@@ -3,207 +3,44 @@
 'use strict';
 
 var util = bcoin.util;
-var body = document.getElementsByTagName('body')[0];
-var log = document.getElementById('log');
-var wdiv = document.getElementById('wallet');
-var tdiv = document.getElementById('tx');
-var floating = document.getElementById('floating');
-var send = document.getElementById('send');
-var newaddr = document.getElementById('newaddr');
-var chainState = document.getElementById('state');
-var rpc = document.getElementById('rpc');
-var cmd = document.getElementById('cmd');
+ 
 var items = [];
 var scrollback = 0;
 var logger, node, wdb;
+var levelsByVal = [
+  'none',
+  'error',
+  'warning',
+  'info',
+  'debug',
+  'spam'
+];
 
-body.onmouseup = function() {
-  floating.style.display = 'none';
-};
-
-floating.onmouseup = function(ev) {
-  ev.stopPropagation();
-  return false;
-};
-
-function show(obj) {
-  floating.innerHTML = escape(util.inspectify(obj, false));
-  floating.style.display = 'block';
+console.html = function (msg){
+  $("#consoleLogs").append("<div class='info'>"+msg+"</div>");
+  $("#consoleLogs").scrollTop($("#consoleLogs")[0].scrollHeight);
 }
+
+console.nhtml = function (level,module,args){
+  $("#networkLogs").append("<div class='info "+levelsByVal[level]+"'>"+levelsByVal[level]+" "+module+args.join("/")+"</div>");
+  // $("#networkLogs").scrollIntoView();
+  $("#networkLogs").scrollTop($("#networkLogs")[0].scrollHeight);
+  // $("#networkLogs").animate({ scrollTop: $("#networkLogs")[0].scrollHeight }, "fast");
+}
+
  
 
 logger = new bcoin.logger({ level: 'debug', console: true });
+logger.open();
 
 logger.writeConsole = function(level, module, args) {
-  console.log(level,module,args);
+  console.nhtml(level,module,args);
 };
 
 logger.info = function(level, module, args) {
-  console.log(level,module,args);
+  console.nhtml(level,module,args);
 };
-
-rpc.onsubmit = function(ev) {
-  var text = cmd.value || '';
-  var argv = text.trim().split(/\s+/);
-  var method = argv.shift();
-  var params = [];
-  var i, arg, param;
-
-  cmd.value = '';
-
-  for (i = 0; i < argv.length; i++) {
-    arg = argv[i];
-    try {
-      param = JSON.parse(arg);
-    } catch (e) {
-      param = arg;
-    }
-    params.push(param);
-  }
-
-  node.rpc.execute({ method: method, params: params }).then(show, show);
-
-  ev.preventDefault();
-  ev.stopPropagation();
-
-  return false;
-};
-
-send.onsubmit = function(ev) {
-  var value = document.getElementById('amount').value;
-  var address = document.getElementById('address').value;
-  var tx, options;
-
-  options = {
-    outputs: [{
-      address: address,
-      value: bcoin.amount.value(value)
-    }]
-  };
-
-  wdb.primary.createTX(options).then(function(mtx) {
-    tx = mtx;
-    return wdb.primary.sign(tx);
-  }).then(function() {
-    return node.sendTX(tx);
-  }).then(function() {
-    show(tx);
-  });
-
-  ev.preventDefault();
-  ev.stopPropagation();
-
-  return false;
-};
-
-newaddr.onmouseup = function() {
-  wdb.primary.createReceive().then(function() {
-    formatWallet(wdb.primary);
-  });
-};
-
-function kb(size) {
-  size /= 1000;
-  return size.toFixed(2) + 'kb';
-}
-
-function create(html) {
-  var el = document.createElement('div');
-  el.innerHTML = html;
-  return el.firstChild;
-}
-
-function escape(html, encode) {
-  return html
-    .replace(!encode ? /&(?!#?\w+;)/g : /&/g, '&amp;')
-    .replace(/</g, '&lt;')
-    .replace(/>/g, '&gt;')
-    .replace(/"/g, '&quot;')
-    .replace(/'/g, '&#39;');
-}
-
-function addItem(item, entry) {
-  var height = entry ? entry.height : -1;
-  var el;
-
-  if (items.length === 20) {
-    el = items.shift();
-    tdiv.removeChild(el);
-    el.onmouseup = null;
-  }
-
-  el = create('<a style="display:block;" href="#'
-    + item.rhash() + '">' + item.rhash() + ' (' + height
-    + ' - ' + kb(item.getSize()) + ')</a>');
-  tdiv.appendChild(el);
-
-  setMouseup(el, item);
-
-  items.push(el);
-
-  chainState.innerHTML = ''
-    + 'tx=' + node.chain.db.state.tx
-    + ' coin=' + node.chain.db.state.coin
-    + ' value=' + bcoin.amount.btc(node.chain.db.state.value);
-}
-
-function setMouseup(el, obj) {
-  el.onmouseup = function(ev) {
-    show(obj);
-    ev.stopPropagation();
-    return false;
-  };
-}
-
-function formatWallet(wallet) {
-  var html = '';
-  var json = wallet.master.toJSON(true);
-  var i, tx, el;
-
-  html += '<b>Wallet</b><br>';
-
-  if (wallet.account.witness) {
-    html += 'Current Address (p2wpkh): <b>'
-      + wallet.getAddress()
-      + '</b><br>';
-    html += 'Current Address (p2wpkh behind p2sh): <b>'
-      + wallet.getNestedAddress()
-      + '</b><br>';
-  } else {
-    html += 'Current Address: <b>' + wallet.getAddress() + '</b><br>';
-  }
-
-  html += 'Extended Private Key: <b>' + json.key.xprivkey + '</b><br>';
-  html += 'Mnemonic: <b>' + json.mnemonic.phrase + '</b><br>';
-
-  wallet.getBalance().then(function(balance) {
-    html += 'Confirmed Balance: <b>'
-      + bcoin.amount.btc(balance.confirmed)
-      + '</b><br>';
-
-    html += 'Unconfirmed Balance: <b>'
-      + bcoin.amount.btc(balance.unconfirmed)
-      + '</b><br>';
-
-    return wallet.getHistory();
-  }).then(function(txs) {
-    return wallet.toDetails(txs);
-  }).then(function(txs) {
-    html += 'TXs:\n';
-    wdiv.innerHTML = html;
-
-    for (i = 0; i < txs.length; i++) {
-      tx = txs[i];
-
-      el = create(
-        '<a style="display:block;" href="#' + tx.hash + '">'
-        + tx.hash + '</a>');
-
-      wdiv.appendChild(el);
-      setMouseup(el, tx.toJSON());
-    }
-  });
-}
+      
 
 var chain = new bcoin.chain({
   db: 'leveldb',
@@ -217,7 +54,8 @@ var chain = new bcoin.chain({
 var pool = new bcoin.pool({
   chain: chain,
   spv: true,
-  maxPeers: 8
+  maxPeers: 8,
+  logger: logger
 });
 
 /*
@@ -228,7 +66,7 @@ var pool = new bcoin.pool({
 
 */
 
-var walletdb = new bcoin.walletdb({ db: 'memory', spv: true });
+var walletdb = new bcoin.walletdb({ db: 'memory', spv: true, logger: logger });
 //1QE8ByvStmhb62zKpKTKujc6FNRqC2RfFV
 //WLTJqUYryqwPS4CKrBVMbMHYZCzhrBVnnezu
 // var xkey = { "xprivkey" : "xprv9s21ZrQH143K2Q8H18WMuiUzhMGpgnWguWoXKpkSEKCNakiSAXV8RfgZwxKh6qU7WCckJGrHkEMxKVKqQC5R1zikyNS5oaQ2n1FcJumPBD3"};
@@ -237,15 +75,15 @@ var walletdb = new bcoin.walletdb({ db: 'memory', spv: true });
 //   return walletdb.open();
 // }).then(function() {
 //   walletdb.getAccounts(0).then(function(res){
-//     console.log(res);
+//     console.html(res);
 //   })
   
 //   var myWallet = new bcoin.wallet.Wallet(walletdb,)
 
 //   // // myWallet.importKey();
   
-//   // console.log(myWallet);
-//   // console.log(myWallet.getAddress());
+//   // console.html(myWallet);
+//   // console.html(myWallet.getAddress());
 // });
 
 
@@ -254,8 +92,9 @@ var walletdb = new bcoin.walletdb({ db: 'memory', spv: true });
 // REAL xprv9s21ZrQH143K2ZVuyZYuv8uFKcGYTsEWCb3cKV1FzQj45oqFiQts2gVZ4qfjfso3J74Snru1ZYAmarb8dHcDwtuWkoPJ6wr8vz269oQCrDp
 
 
+
 pool.open().then(function() {
-  return walletdb.open();
+  return walletdb.open({logger:logger});
 }).then(function() {
   return walletdb.create({
     "master": "xprv9s21ZrQH143K2ZVuyZYuv8uFKcGYTsEWCb3cKV1FzQj45oqFiQts2gVZ4qfjfso3J74Snru1ZYAmarb8dHcDwtuWkoPJ6wr8vz269oQCrDp",
@@ -264,40 +103,46 @@ pool.open().then(function() {
 
   // return walletdb.create();
 }).then(function(wallet) {
-  console.log(wallet);
-  // console.log(walletdb.toJSON());
-  console.log('Main address: %s', wallet.getAddress('base58'));
+  // console.html(wallet);
+  // console.html(walletdb.toJSON());
+  console.html("Root m/0/0/0 => "+wallet.getID());
+
+  console.html('Main address: '+ wallet.getAddress('base58check'));
   
-  // console.log(wallet.master.key.toJSON());
-  wallet.account.receiveDepth = 3;
-   
-  console.log("R => ",wallet.account.receiveDepth);
+  // console.html(wallet.master.key.toJSON());
+  // wallet.account.receiveDepth = 3;
+
+  // console.html("R => ",wallet.getID());
+  // console.html("receive => ",wallet.account.receive);
 
   
-  wallet.createKey(0).then(function (res){
-    console.log("NEW", res.getAddress('base58'),res);
-    wallet.createKey(0).then(function (res){
-      console.log("NEW", res.getAddress('base58'),res);
-      wallet.createKey(0).then(function (res){
-        console.log("NEW", res.getAddress('base58'),res);
-        console.log("R => ",wallet.account.receiveDepth);
-      });
-    });
+
+  // console.html(logger);
+  
+  wallet.getAccountPaths(0).then(function(result){
+    var a;
+    for (var i in result){
+      a =result[i].toAddress();
+
+      // console.html("Paths======>",a.toString());
+    }
   });
-
+  pool.watchAddress(wallet.getAddress('base58check'));
   
 
+  // wallet.getBalance(0).then(function(result){
+  //     console.html("Balance======>",result);
+  // });
+
+  for (var i=0;i<10;i++){
+    wallet.createKey(0).then(function (res){
+      pool.watchAddress(res.getAddress('base58check'));  
+      console.html("watching " + res.getAddress('base58check'));
+    });
+  }
   
-   
-   
-
-  
-
-
-
-
   // Add our address to the spv filter.
-  pool.watchAddress(wallet.getAddress());
+  
 
   // Connect, start retrieving and relaying txs
   pool.connect().then(function() {
@@ -309,8 +154,8 @@ pool.open().then(function() {
     });
 
     wallet.on('balance', function(balance) {
-      console.log('Balance updated.');
-      console.log(bcoin.amount.btc(balance.unconfirmed));
+      console.html('Balance updated.');
+      console.html(bcoin.amount.btc(balance.unconfirmed));
     });
   });
 });
